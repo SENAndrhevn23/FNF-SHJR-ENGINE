d0f13a7aebc48b60adb62f7b7b5e2c4f01722c1c
Andre Nicholas Jr <pumpgaming2@gmail.com>
2026-04-14 11:52:16 -0500

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -14,7 +14,7 @@ enum MainMenuColumn {
 
 class MainMenuState extends MusicBeatState
 {
-    public static var shjrVersion:String = '0.0.1'; // SHJR Engine Version
+    public static var shjrVersion:String = '0.0.2'; // SHJR Engine Version
 	public static var psychEngineVersion:String = '1.0.4'; // This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 	public static var curColumn:MainMenuColumn = CENTER;
